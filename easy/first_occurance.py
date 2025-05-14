c62f4a281d622123ca97905538d3e9c2e2f601d7
Mihir <anuj.nits@gmail.com>
2025-05-14 21:14:28 +0530

    next challenge

---
+++ easy/first_occurance.py
@@ -0,0 +1,13 @@
+# https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/
+
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        if needle in haystack:
+            return haystack.index(needle)
+        else:
+            return -1
+
+
+s = Solution()
+r = s.strStr("sadbutsad", "sad")
+print(r)
